2ecf0d77813f0223f652e867be49792fde58eeec
mrdrx27 <argafauzianto27@gmail.com>
2021-02-10 21:36:04 +0700

    second commit

---
+++ main.js
@@ -0,0 +1,27 @@
+//perulangan while
+//syarat while: memiliki nilai awal, while(kondisi terminasi), aksi, increment, dan decrement 
+
+// var apel = 1;
+
+// while (apel <=10) {
+//     console.log('buah apel No.' + apel + 'yang terbaik.');
+// apel++;    
+// }
+
+//for javascript
+
+var jmlBus = 20;
+
+var busBeroperasi = 16;
+
+var noBus = 1;
+
+while (noBus <= busBeroperasi) {
+    console.log('Bus No.' + noBus + 'Kondisi sangat baik dan Aman');
+
+noBus++;    
+}
+
+for( noBus = 17; noBus <= jmlBus; noBus++ ) {
+    console.log('Bus No.' + noBus + 'Sedang dalam maintenance service')
+}
